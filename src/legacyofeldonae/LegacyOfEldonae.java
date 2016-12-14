e4d87f64507364b60ce32542c0b67a62da83743a
Home <Home@Home-PC.lan>
2016-12-14 23:29:45 +0100

    Revert "Merge origin/master"
    This reverts commit 605ad32e8ed67228064200a2b11f4890bc848ce6.

--- src/legacyofeldonae/LegacyOfEldonae.java
+++ src/legacyofeldonae/LegacyOfEldonae.java
@@ -20,6 +20,7 @@ public class LegacyOfEldonae {
         // TODO code application logic here
         JOptionPane.showMessageDialog(null, "Test", "Test Commit", JOptionPane.INFORMATION_MESSAGE);
         JOptionPane.showMessageDialog(null, "Test 2", "Test Commit", JOptionPane.INFORMATION_MESSAGE);
+        JOptionPane.showMessageDialog(null, "Test 3", "Test Commit", JOptionPane.INFORMATION_MESSAGE);
 
     }
     
